5710c641b8f993e0a1dfa92508c1bd09857cd723
jamilton06 <jamiltonmeneses06@gmail.com>
2014-09-03 16:32:25 -0500

    clases

---
+++ parqueaderovs/parqueadero/Contabilidad/MovimientoContable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace parqueaderovs.parqueadero.Contabilidad
+{
+    public class MovimientoContable
+    {
+    }
+}
